f024aae42e744f47d255fd0b53a3d5e283be86c9
Confikt <confikt@gmail.com>
2015-10-15 15:40:05 +0200

    Javadoc on test cases

--- src/ObjectsForTest/KomponentTest.java
+++ src/ObjectsForTest/KomponentTest.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 
 public class KomponentTest {
 
+	/**
+	 * Saves and load and test stream of data with a Komponent object.
+	 */
 	@Test
 	public void loadAndSaveTest() {
 		TableModel dataModel = new TableModel();
@@ -29,25 +32,35 @@ public class KomponentTest {
             assertEquals("", dataModel1.getValueAt(0, 1));
             assertEquals(1, dataModel1.getValueAt(0, 5));
         } catch (Exception e) {
-            fail(e.getMessage());    	
+            fail(e.getMessage());
         }
 	}
 	
+	/**
+	 * Test of TableModel variables. 
+	 */
 	@Test
-    public void testConstructor() {
+    public void testTableModel() {
         TableModel dataModel = new TableModel();
         assertEquals(0, dataModel.getRowCount());
         assertEquals(9, dataModel.getColumnCount());
         assertEquals("Row", dataModel.getColumnName(3));
     }
 
+	/**
+	 * Initialize constructor of komponent and checks that it gets added to the vector
+	 */
     @Test
-    public void testInsertData() {
+    public void testConstructor() {
     	TableModel dataModel = new TableModel();
         dataModel.append(new Komponent());
         assertEquals(1, dataModel.getRowCount());
     }
 
+    /**
+     * Make a new Komponent in the vector and sets the Komponent type and
+     * checks that the correct type is set
+     */
     @Test
     public void testGetValueAt() {
     	TableModel dataModel = new TableModel();
